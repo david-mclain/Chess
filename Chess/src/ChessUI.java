0945a32e4c4ea9c3b64a31c693323293b48a7551
david-mclain <92353763+david-mclain@users.noreply.github.com>
2022-11-18 21:52:31 -0700

    Updated UI look and feel

--- Chess/src/ChessUI.java
+++ Chess/src/ChessUI.java
@@ -1,9 +1,11 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ChessUI extends JFrame {
@@ -20,8 +22,39 @@ public class ChessUI extends JFrame {
 		board = new Board();
 		setLayout(null);
 		add(getBoard());
+		Font font = new Font("Monospaced", Font.PLAIN, 40);
+		for (int i = 0; i < 8; i++) {
+			JLabel num = new JLabel();
+			num.setSize(40, 40);
+			num.setFont(font);
+			num.setText("" + (8 - i));
+			num.setBounds(15, 80 + i * 100, 40, 40);
+			num.setForeground(Color.BLACK);
+			add(num);
+			JLabel num2 = new JLabel();
+			num2.setSize(40, 40);
+			num2.setFont(font);
+			num2.setText("" + (8 - i));
+			num2.setBounds(865, 80 + i * 100, 40, 40);
+			num2.setForeground(Color.BLACK);
+			add(num2);
+			JLabel let = new JLabel();
+			let.setSize(40, 40);
+			let.setFont(font);
+			let.setText("" + (char)(i + 65));
+			let.setBounds(90 + i * 100, 10, 40, 40);
+			let.setForeground(Color.BLACK);
+			add(let);
+			JLabel let2 = new JLabel();
+			let2.setSize(40, 40);
+			let2.setFont(font);
+			let2.setText("" + (char)(i + 65));
+			let2.setBounds(90 + i * 100, 850, 40, 40);
+			let2.setForeground(Color.BLACK);
+			add(let2);
+		}
 		setVisible(true);
-		setSize(900, 925);
+		setSize(925, 950);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
@@ -34,7 +67,6 @@ public class ChessUI extends JFrame {
 		board.setBounds(50, 50, 800, 800);
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-				JPanel panel = new JPanel();
 				Color color;
 				BoardSquare cur;
 				final int row = i;
